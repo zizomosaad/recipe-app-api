e24e04f9e85b39059e57d3b469598b5984f40483
Ziad Mosaad <z.mossad@trasntecbds.com>
2025-09-12 22:13:16 +0300

    configure list , change and add user pages in django admin panel and add api documentation schema and create gui (swagger)

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,43 @@
+"""Test for the Django admin modifications"""
+
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Tests for django admin"""
+
+    def setUp(self):
+        """Create user and client before running any tests."""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com", password="testpass123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@example.com", password="testpass123", name="user"
+        )
+
+    def test_users_list(self):
+        """Test that users are listed on page."""
+
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse("admin:core_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
