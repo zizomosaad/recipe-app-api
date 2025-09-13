f436a4df4b030d678a53b80de5a7e6dbc645da61
Ziad Mosaad <z.mossad@trasntecbds.com>
2025-09-13 19:34:05 +0300

    handle recipe detail api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,14 +7,21 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Recipe
 from recipe import serializers
 
+
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs"""
 
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        return self.queryset.filter(user=self.request.user).order_by("-id")
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == "list":
+            return serializers.RecipeSerializer
+        return self.serializer_class
